28384bda97a826b013bba18711d255aafe2f0f5d
Subhadeep0506 <subhadeepdoublecap@gmail.com>
2021-05-20 21:14:56 +0530

    Updated BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-
 public class BubbleSort{
 
     public int[] bubbleSort(int[] array){
         int temp = 0;
+
         for(int i = 0; i < array.length; i++){
             for(int j = 0; j < array.length - i - 1; j++){
                 if(array[j] > array[j + 1]){
@@ -13,6 +13,7 @@ public class BubbleSort{
                 }
             }
         }
+
         return array;
     }
 
@@ -21,17 +22,20 @@ public class BubbleSort{
         System.out.println("Enter no of elements you want to enter: ");
         int size = sc.nextInt();
         int array[] = new int[size];
-        
+
         System.out.println("Enter the elements: ");
         for(int i = 0; i < size; i++){
             array[i] = sc.nextInt();
         }
+
         BubbleSort obj_1 = new BubbleSort();
         array = obj_1.bubbleSort(array);
+
         System.out.println("Sorted elements: ");
         for(int i = 0; i < 6; i++){
             System.out.print(array[i] + " ");
         }
+
         sc.close();
     }
 }

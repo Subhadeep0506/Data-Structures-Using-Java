4752d9120e589b1573813acb053a0ff8d7185453
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2021-08-13 09:56:11 +0530

    renamed graph

---
+++ data_structures/examples/graph_adjacency_matrix/Graph.java
@@ -0,0 +1,114 @@
+package data_structures.examples.graph_adjacency_matrix;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Graph {
+    ArrayList<GraphNode> nodeList = new ArrayList<>();
+    int[][] adjacencyMatrix;
+
+    public Graph(ArrayList<GraphNode> nodeList) {
+        this.nodeList = nodeList;
+        adjacencyMatrix = new int[nodeList.size()][nodeList.size()];
+    }
+
+    // calling this function creates a link between 1st node and 2nd node
+    public void addUndirectedEdge(int i /*index of 1st node*/,int j /*index of 2nd node*/) {
+        // since edges are undirected, if one node has connection with another node, 
+        // then that node has return connection with the first node
+        adjacencyMatrix[i][j] = 1;
+        adjacencyMatrix[j][i] = 1;
+    }
+
+    // prints the adjacency matrix
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("  ");
+        for (int i = 0; i < nodeList.size(); i++) {
+            sb.append(nodeList.get(i).name + " ");
+        }
+        sb.append("\n");
+        for (int i = 0; i < nodeList.size(); i++) {
+            sb.append(nodeList.get(i).name + " ");
+            for (int j : adjacencyMatrix[i]) {
+                sb.append(j + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    // get neighbors
+    public ArrayList<GraphNode> getNeighbors(GraphNode node) {
+        ArrayList<GraphNode> neighbors = new ArrayList<>();
+        int nodeIndex = node.index;
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[nodeIndex][i] == 1)
+                neighbors.add(nodeList.get(i));
+        }
+
+        return neighbors;
+    }
+
+    // BFS internal
+    private void bfsVisit(GraphNode node) {
+        LinkedList<GraphNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            GraphNode currentNode = queue.remove(0);
+            currentNode.isVisited = true;
+            System.out.print(currentNode.name + " ");
+            ArrayList<GraphNode> neighbors = getNeighbors(currentNode);
+            for (GraphNode neighbor : neighbors) {
+                if (!neighbor.isVisited) {
+                    queue.add(neighbor);
+                    neighbor.isVisited = true;
+                }
+            }
+        }
+    }
+
+    // callable bfs method
+    public void bfs() {
+        for (GraphNode node : nodeList) {
+            if (!node.isVisited)
+                bfsVisit(node);
+        }
+        setAllNotVisited();
+    }
+
+    // dfs internal method
+    private void dfsVisit(GraphNode node) {
+        Stack<GraphNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            GraphNode currentNode = stack.pop();
+            currentNode.isVisited = true;
+            System.out.print(currentNode.name + " ");
+            ArrayList<GraphNode> neighbors = getNeighbors(node);
+            for (GraphNode neighbor : neighbors) {
+                if (!neighbor.isVisited) {
+                    stack.push(neighbor);
+                    neighbor.isVisited = true;
+                }
+            }
+        }
+    }
+
+    // callable dfs method
+    public void dfs() {
+        for (GraphNode node : nodeList) {
+            if (!node.isVisited)
+                dfsVisit(node);
+        }
+        setAllNotVisited();
+    }
+
+    // sets all node to unvisited
+    private void setAllNotVisited() {
+        for (GraphNode node : nodeList) {
+            node.isVisited = false;
+        }
+    }
+}
---
+++ data_structures/examples/graph_adjacency_matrix/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Implementing graph using adjacency matrix
+ */
+
+package data_structures.examples.graph_adjacency_matrix;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<GraphNode> nodeList = new ArrayList<>();
+        nodeList.add(new GraphNode("A", 0));
+        nodeList.add(new GraphNode("B", 1));
+        nodeList.add(new GraphNode("C", 2));
+        nodeList.add(new GraphNode("D", 3));
+        nodeList.add(new GraphNode("E", 4));
+
+        Graph graph = new Graph(nodeList);
+        graph.addUndirectedEdge(0, 1);
+        graph.addUndirectedEdge(0, 2);
+        graph.addUndirectedEdge(0, 3);
+        graph.addUndirectedEdge(1, 4);
+        graph.addUndirectedEdge(2, 3);
+        graph.addUndirectedEdge(3, 4);
+
+        System.out.println(graph.toString());
+        graph.bfs();
+        System.out.println();
+        graph.dfs();
+    }
+}
